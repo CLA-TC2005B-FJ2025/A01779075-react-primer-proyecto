51a14b8521c4e2a7d01c5e444236040f16ebc872
Sil <a01779075@tec.mx>
2025-03-28 09:23:25 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Tarjeta } from './Tarjeta'
+
+export function App() {
+  return (
+    <div>
+        <div>Personajes de Mario Bros</div>
+        <Tarjeta/>
+        <Tarjeta/>
+        <Tarjeta/>
+    </div>
+  )
+}
---
+++ src/Tarjeta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Tarjeta() {
+  return (
+    <div>
+        Tarjeta de Personaje
+    </div>
+  )
+}
