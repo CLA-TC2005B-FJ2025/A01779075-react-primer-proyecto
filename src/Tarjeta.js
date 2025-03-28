f7fcaf5a38a98d3d9e8c77d2c96d036c200e5532
Sil <a01779075@tec.mx>
2025-03-28 16:02:21 +0000

    aaaaaaaaaaa

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -3,9 +3,9 @@ import React from 'react'
 export function Tarjeta(props) {
   return (
     <div>
+        <img src={props.image} width={150}/>
         <div>Npmbre del personaje: {props.nombre}</div>
         <div>Color favorito: {props.colorfavorito}</div>
-        <div><div/>
     </div>
   )
 }
