0f102a714a7afe33fadb1a8343a48a32aded2bec
Sil <a01779075@tec.mx>
2025-03-28 16:07:04 +0000

    aaaaaaaaaaaaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+// import CSS
+import 'bulma/css/bulma.css'
+
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
-import MarioImage from "/imagenes/Mario.png"
-import LuigiImage from "/imagenes/Luigi.png"
-import PeachImage from "/imagenes/Peaches.png"
+// Importamos las imagenes
+import MarioImage from './imagenes/Mario.png'
+import LuigiImage from './imagenes/Luigi.png'
+import PeachesImage from './imagenes/Peaches.png'
 
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros</div>
-        <Tarjeta nombres="Mario" colorfavorito="Rojo" image={MarioImage}/>
-        <Tarjeta nombres="Luigi" colorfavorito="Verde" image={LuigiImage}/>
-        <Tarjeta nombre="Peach" colorfavorito="Rosa" image={PeachImage}/>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-2">
+                <Tarjeta nombre="Mario" colorFavorito="Rojo" image={MarioImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Luigi" colorFavorito="Verde" image={LuigiImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Peaches" colorFavorito="Rosa" image={PeachesImage}/>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
+  
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-export function Tarjeta(props) {
+export function Tarjeta( props ) {  // se le puede poner otro nombre
   return (
-    <div>
-        <img src={props.image} width={150}/>
-        <div>Npmbre del personaje: {props.nombre}</div>
-        <div>Color favorito: {props.colorfavorito}</div>
-    </div>
+    <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.image} alt="pda logo" />
+          </figure>
+        </div>
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{props.nombre}</p>
+            <p className="subtitle is-6">{props.colorFavorito} es mi color favorito</p>
+          </div>
+        </div>
+      </div>
+  
   )
 }
